2c96b8647f9825ef225f18e8f5a673f3c218abba
blykrise <blykrise@gmail.com>
2024-04-06 22:36:25 +0100

    Reset commit

---
+++ api/server.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="../frontend/static"), name="static")
+
+templates = Jinja2Templates(directory="../frontend")
+
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse(
+            request=request, name="index.html", context={}
+            )
+
+
